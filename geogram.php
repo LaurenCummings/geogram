89f213a083768ad1e4f842c5fed06c133ba13543
LaurenCummings <LaurenCummings@users.noreply.github.com>
2016-04-05 23:54:49 -0400

    Added files via upload

--- geogram.php
+++ geogram.php
@@ -30,7 +30,7 @@ if(!empty($_GET['location'])) {
 			<br/>
 			<?php
 			if(!empty($instagram_array)){
-				foreach($instagram_array['data'] as $image){
+				foreach($instagram_array['data'] as $key=>$image){
 					echo '<img src="'.$image['images']['low_resolution']['url'].'" alt="" />';
 				}
 			}
